d59c9e3230f23c82fe2cc47816374d3787fdb182
Igor Frolov <darklol_777@mail.ru>
2019-09-01 19:41:31 +0300

    checkbox-buttons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './components/masked-text-field/masked-text-field.js'
 import './components/field-button/field-button.js';
 import './components/switch/switch.js';
 import './components/radio-buttons/radio-buttons.js';
+import './components/checkbox-buttons/checkbox-buttons.js';
