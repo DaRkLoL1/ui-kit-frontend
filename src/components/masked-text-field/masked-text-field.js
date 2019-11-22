2681be0a2ddce50620a4a80299f2ea81eaa166fd
Igor Frolov <darklol_777@mail.ru>
2019-11-22 19:04:31 +0300

    Поля формы #26

--- src/components/masked-text-field/masked-text-field.js
+++ src/components/masked-text-field/masked-text-field.js
@@ -6,7 +6,7 @@ export default class Mask {
   }
 
   createMask() {
-    $('.js-mask').mask('AB.SR.YXOQ', {
+    $('.js-mask').mask('AB.SR.YXOK', {
       translation: {
         A: { pattern: /[0-3]/ },
         B: { pattern: /[0-9]/ },
@@ -30,7 +30,7 @@ export default class Mask {
   static createMaskOption(val) {
     if (Number.parseInt(val[6], 10) === 1) {
       return {
-        O: { pattern: /[5-9]/ },
+        O: { pattern: /[0-9]/ },
         X: { pattern: /[9]/ },
         A: { pattern: /[0-3]/ },
         B: { pattern: /[0-9]/ },
@@ -44,7 +44,7 @@ export default class Mask {
       return {
         R: { pattern: /[0-2]/ },
         A: { pattern: /[0-3]/ },
-        B: { pattern: /[0-1]/ },
+        B: { pattern: /[0-9]/ },
         S: { pattern: /[0-1]/ },
         Y: { pattern: /[1-2]/ },
         X: { pattern: /[0]/ },
@@ -54,8 +54,8 @@ export default class Mask {
     }
     if (Number.parseInt(val[0], 10) === 0) {
       return {
-        A: { pattern: /[0-3]/ },
         B: { pattern: /[1-9]/ },
+        A: { pattern: /[0-3]/ },
         S: { pattern: /[0-1]/ },
         R: { pattern: /[1-9]/ },
         Y: { pattern: /[1-2]/ },
@@ -66,8 +66,8 @@ export default class Mask {
     }
     if (Number.parseInt(val[0], 10) === 3) {
       return {
-        A: { pattern: /[0-3]/ },
         B: { pattern: /[0-1]/ },
+        A: { pattern: /[0-3]/ },
         S: { pattern: /[0-1]/ },
         R: { pattern: /[1-9]/ },
         Y: { pattern: /[1-2]/ },
@@ -76,10 +76,9 @@ export default class Mask {
         K: { pattern: /[0-9]/ },
       };
     }
-
     return {
-      B: { pattern: /[0-9]/ },
       A: { pattern: /[0-3]/ },
+      B: { pattern: /[0-9]/ },
       S: { pattern: /[0-1]/ },
       R: { pattern: /[1-9]/ },
       Y: { pattern: /[1-2]/ },
